de94f72ce8e445024d2d211a32c1996abb06af4f
Georgmany Ruiz <37196080+GeorgmanyRuiz@users.noreply.github.com>
2021-12-23 05:54:31 -0500

    first commit

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
-const userScore = 0;
-const computerScore =0;
+let userScore = 0;
+let computerScore =0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const fire_div = document.getElementById("f");
 const water_div = document.getElementById("w");
 const grass_div = document.getElementById("g");
+
 
 function getComputerChoice() {
   const choices = ["f", "w", "g"];
   const randomNumber = Math.floor(Math.random() * 3);
   return choices[randomNumber];
+}
+
+function convertToWord(letter) {
+  if (letter === "f") return "Fire";
+  if (letter === "w") return "Water"
+  if (letter === "g") return "Grass"
+}
+
+function win(userChoice, computerChoice) {
+  const userChoice_div = document.getElementById(userChoice);
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}. You Win!!`;
+  userChoice_div.classList.add('green-glow');
+  setTimeout(() => userChoice_div.classList.remove('green-glow'), 1000);
+}
+
+
+function lose(userChoice, computerChoice) {
+  const userChoice_div = document.getElementById(userChoice);
+  computerScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `${convertToWord(userChoice)} loses to ${convertToWord(computerChoice)}. You Lost :(...`;
+  userChoice_div.classList.add('red-glow');
+  setTimeout(() => userChoice_div.classList.remove('red-glow'), 600);
+}
+
+function draw(userChoice, computerChoice) {
+  const userChoice_div = document.getElementById(userChoice);
+  result_p.innerHTML = `${convertToWord(userChoice)} does nothing to ${convertToWord(computerChoice)}. It's a Draw?!`;
+  userChoice_div.classList.add('gray-glow');
+  setTimeout(() => userChoice_div.classList.remove('gray-glow'), 600);
 }
 
 function game(userChoice){
@@ -20,33 +55,35 @@ function game(userChoice){
     case "fg":
     case "wf":
     case "gw":
-      console.log("USER WINS!");
+      win(userChoice, computerChoice);
       break;
     case "gf":
     case "wg":
     case "fw":
-      console.log("USER LOSES..");
+      lose(userChoice, computerChoice);
       break;
     case "ff":
     case "ww":
     case "gg":
-      console.log("Its A Draw..");
+      draw(userChoice, computerChoice);
       break;
   }
 }
 
+var myAudio = document.getElementById("audio1");
+  myAudio.onloadeddata = function() {
+  myAudio.volume = 0.15;
+};
+
+
+
+
 function main() {
-  fire_div.addEventListener("click", function() {
-    game("f");
-  })
+  fire_div.addEventListener("click", () => game("f"));
 
-  water_div.addEventListener("click", function() {
-    game("w");
-  })
+  water_div.addEventListener("click", () => game("w"));
 
-  grass_div.addEventListener("click", function() {
-    game("g");
-  })
+  grass_div.addEventListener("click", () => game("g"));
 }
 
 main();
